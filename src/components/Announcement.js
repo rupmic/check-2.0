bceea021f33bec613e5d9daf9221ef2ebd137cca
Michał Rupiński <rupmic@wp.pl>
2023-08-02 23:59:28 +0200

    modified

--- src/components/Announcement.js
+++ src/components/Announcement.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../styles/Announcement.module.scss";
 
+import Cookies from "universal-cookie";
+
+import { BiShow, BiHide } from "react-icons/bi";
+
 const Announcement = () => {
+  const [hide, setHide] = useState(true);
+
+  const toggleVisability = () => {
+    setHide(!hide);
+  };
+
   return (
-    <div className={styles.announcement_wrapper}>
+    <div
+      className={`${styles.announcement_wrapper} ${hide ? styles.hide : null}`}
+    >
       <p className={styles.title}>
         Nowa wersja modułu wyszukiwania czasopism. Zapraszamy do testowania!
       </p>
@@ -13,8 +25,12 @@ const Announcement = () => {
       </p>
       <p className={styles.p_block}>Dziękujemy za zaufanie!</p>
       <p className={styles.p_block}>
-        Jednoosobowy zespół techniczny<span className={styles.icon}>:)</span>
+        Jednoosobowy zespół techniczny
+        <span className={styles.icon_smile}>:)</span>
       </p>
+      <button className={styles.icon_close} onClick={() => toggleVisability()}>
+        {hide ? <BiShow /> : <BiHide />}
+      </button>
     </div>
   );
 };
