739bf5d00130dcf7482ef45d62f611464c658259
Michał Rupiński <rupmic@wp.pl>
2024-01-08 22:02:23 +0100

    implement GTM and GA | cleanup code

--- src/index.js
+++ src/index.js
@@ -4,6 +4,14 @@ import "./styles/base/_global.scss";
 
 import { App } from "./App";
 
+import TagManager from "react-gtm-module";
+
+const tagManagerArgs = {
+  gtmId: "GTM-P43QFPZX",
+};
+
+TagManager.initialize(tagManagerArgs);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
